76b76484776be19384422356bdcc0631d13daf47
tunjiNg01 <nahtlosng@gmail.com>
2023-09-12 10:23:55 +0100

    bug fixes

--- components/ContactSection.jsx
+++ components/ContactSection.jsx
@@ -6,7 +6,7 @@ export default function Contact() {
       <div className=" grid grid-cols-1 sm:grid-cols-3 sm:space-x-24">
         <div className="">
           <h1 className="fon-raleway text-5xl font-extrabold sm:text-7xl">
-            Let's <br /> work together!
+            Let&apos;s <br /> work together!
           </h1>
           <p className="mt-8 font-raleway">
             Got a question or proposal, or just want to say hello? Go ahead.
